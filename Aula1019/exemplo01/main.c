0365886effbc33b6dd86cfdc9b55ce7de8c04b40
empucrs <edson.moreno@pucrs.br>
2023-10-19 13:04:17 -0300

    aula introdutoria sobre gpio e botoes

--- Aula1019/exemplo01/main.c
+++ Aula1019/exemplo01/main.c
@@ -6,12 +6,31 @@
 int main(void)
 {
     DDRB |= (1 << PB5);       // seta PB5 como saída
+    DDRB &= ~(1 << PB0);      // seta PB0 como entrada
+    DDRB &= ~(1 << PB1);      // seta PB1 como entrada
+    
+    int estado=0;
 
     while (1)
     {
-        PORTB |= (1 << PB5);  // liga LED
-        _delay_ms(500);
-        PORTB &= ~(1 << PB5); // desliga LED
-        _delay_ms(500);
+		int valorDoBotaoPB0 = (PINB & (1<<PB0));
+		int valorDoBotaoPB1 = (PINB & (1<<PB1));
+		
+		if((valorDoBotaoPB0!=0)||(valorDoBotaoPB1==0)){
+			if(estado==0)
+				estado=1;
+			else
+				estado=0;
+		}
+
+		if (estado==0)
+			PORTB &= ~(1 << PB5); // desliga LED
+		else
+			PORTB |= (1 << PB5);  // liga LED
+			
+		while( ((PINB & (1<<PB0)) != 0) ) _delay_ms(1); // tratar o pb0
+		while( ((PINB & (1<<PB1)) == 0) ) _delay_ms(1); // tratar o pb1
+        
+        _delay_ms(1);
     }
 }
